c8834b6dd51b3b78fb7ed7396dc84a098714d108
hot666666 <hotcs6@naver.com>
2024-05-30 14:30:11 +0900

    Initial Commit

---
+++ CocoaTalk/CocoaTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaTalkApp.swift
+//  CocoaTalk
+//
+//  Created by 최하식 on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CocoaTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
