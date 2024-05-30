8d04f24fed2a5e47523796e1cb29540746d13cd4
hot666666 <hotcs6@naver.com>
2024-05-30 19:40:37 +0900

    feat : create LoginView - refactor LoginButtonStyle

---
+++ CocoaTalk/Presentation/Login/LoginButtonStyle.swift
@@ -0,0 +1,31 @@
+//
+//  LoginButtonStyle.swift
+//  CocoaTalk
+//
+//  Created by 최하식 on 5/30/24.
+//
+
+import SwiftUI
+
+struct LoginButtonStyle: ButtonStyle {
+    let textColor: Color
+    let borderColor: Color
+    
+    init(textColor: Color, borderColor: Color? = nil) {
+        self.textColor = textColor
+        self.borderColor = borderColor ?? textColor
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.body)
+            .foregroundColor(textColor)
+            .frame(maxWidth: .infinity, maxHeight: 40)
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(borderColor, lineWidth: 1)
+            }
+            .padding(.horizontal, 15)
+            .opacity(configuration.isPressed ? 0.5 : 1)
+    }
+}
---
+++ CocoaTalk/Presentation/Login/LoginView.swift
@@ -0,0 +1,104 @@
+//
+//  LoginView.swift
+//  CocoaTalk
+//
+//  Created by 최하식 on 5/30/24.
+//
+
+import SwiftUI
+
+struct LoginIntroView: View {
+    @State var isPresented: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack{
+                Spacer()
+                
+                VStack(spacing: 20){
+                    Text("환영합니다.")
+                        .font(.title)
+                        .bold()
+                    Text("무료 코코아톡을 부담없이 즐겨보세요!")
+                        .foregroundColor(.secondary)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    isPresented.toggle()
+                }, label: {
+                    Text("로그인")
+                        .bold()
+                })
+                .buttonStyle(LoginButtonStyle(textColor: .yellow))
+            }
+            .navigationDestination(isPresented: $isPresented) {
+                LoginView()
+            }
+        }
+    }
+}
+
+struct LoginView: View {
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var vm: AuthenticationViewModel
+    
+    var body: some View {
+        VStack{
+            HStack{
+                VStack(alignment: .leading, spacing: 20){
+                    Text("로그인")
+                        .font(.title)
+                        .bold()
+                    Text("아래 제공되는 서비스로 로그인 해주세요.")
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+            }
+            .padding()
+            
+            Spacer()
+            
+            Button(action: {
+                Task {
+                    await vm.send(action: .googleLogin)
+                }
+            }, label: {
+                Text("Google로 로그인")
+            })
+            .buttonStyle(LoginButtonStyle(textColor: .black, borderColor: .gray))
+        }
+        .overlay {
+            if vm.isLoading {
+                ZStack{
+                    Color.black.opacity(0.1)
+                    ProgressView()
+                }
+                .ignoresSafeArea()
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.secondary)
+                })
+            }
+        }
+    }
+}
+
+#Preview("LoginIntro") {
+    LoginIntroView()
+}
+
+#Preview("Login") {
+    NavigationView {
+        LoginView()
+            .environmentObject(AuthenticationViewModel(container: .stub))
+    }
+}
