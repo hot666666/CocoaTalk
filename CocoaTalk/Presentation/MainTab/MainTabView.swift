5d3f070242d38b512dc39e78cd868074af18e98d
hot666666 <hotcs6@naver.com>
2024-06-01 20:38:59 +0900

    feat : create MainTabView

---
+++ CocoaTalk/Presentation/MainTab/MainTabView.swift
@@ -0,0 +1,80 @@
+//
+//  MainTabView.swift
+//  CocoaTalk
+//
+//  Created by 최하식 on 5/31/24.
+//
+
+import SwiftUI
+
+enum MainTabType: String, CaseIterable {
+    case home = "친구"
+    case chat = "채팅"
+    case more = "더보기"
+    
+    var title: String {
+        self.rawValue
+    }
+    
+    func imageName(selected: Bool = false) -> String {
+        switch self {
+        case .home:
+            return selected ? "person.fill" : "person"
+        case .chat:
+            return selected ? "message.fill" : "message"
+        case .more:
+            return "ellipsis"
+        }
+    }
+}
+
+struct MainTabView: View {
+    @EnvironmentObject var authViewModel: AuthenticationViewModel
+    @EnvironmentObject var container: DIContainer
+    @State private var selectedTab: MainTabType = .home
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            ForEach(MainTabType.allCases, id: \.self) { tab in
+                Group {
+                    switch tab {
+                    case .home:
+                        HomeView(vm: .init(container: container, userId: authViewModel.userId!))
+                    case .chat:
+                        Color.white
+                            .overlay {
+                                Button(action: {
+                                    Task {
+                                        await authViewModel.send(action: .logout)
+                                    }
+                                }) {
+                                    Text("로구아웃")
+                                }
+                            }
+                    case .more:
+                        Color.blue
+                    }
+                }
+                .tabItem {
+                    Label(tab.title, systemImage: tab.imageName(selected: selectedTab == tab))
+                }
+                .tag(tab)
+            }
+        }
+        .tint(.primary)
+    }
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.gray.withAlphaComponent(0.07)
+    }
+}
+
+#Preview {
+    let container: DIContainer = .stub
+    let vm = AuthenticationViewModel(container: container)
+    vm.userId = "loggedInUser"
+    
+    return MainTabView()
+        .environmentObject(container)
+        .environmentObject(vm)
+}
