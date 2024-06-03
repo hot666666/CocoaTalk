7c9fa5aa486e5f175aa6000fda22655d68347346
hot666666 <hotcs6@naver.com>
2024-06-03 20:49:48 +0900

    fix : update Services

--- CocoaTalk/Service/Services.swift
+++ CocoaTalk/Service/Services.swift
@@ -10,22 +10,32 @@ import Foundation
 protocol ServiceType {
     var authService: AuthenticationServiceType { get set }
     var userService: UserServiceType { get set }
+    var photoPickerService: PhotoPickerServiceType { get set }
+    var uploadService: UploadServiceType { get set }
 }
 
 class Services: ServiceType {
     var authService: AuthenticationServiceType
     var userService: UserServiceType
+    var photoPickerService: PhotoPickerServiceType
+    var uploadService: UploadServiceType
     
     init(
         authService: AuthenticationServiceType = AuthenticationService(),
-        userService: UserServiceType = UserService(dbRepository: UserDBRepository())
+        userService: UserServiceType = UserService(dbRepository: UserDBRepository()),
+        photoPickerService: PhotoPickerServiceType = PhotoPickerService(),
+        uploadService: UploadServiceType = UploadService(provider: UploadProvider())
     ) {
         self.authService = authService
         self.userService = userService
+        self.photoPickerService = photoPickerService
+        self.uploadService = uploadService
     }
 }
 
 class StubServices: ServiceType {
     var authService: AuthenticationServiceType = StubAuthenticationService()
     var userService: UserServiceType = StubUserService()
+    var photoPickerService: PhotoPickerServiceType = StubPhotoPickerService()
+    var uploadService: UploadServiceType = StubUploadService()
 }
