3ddfccad5053fd83798ad8a1a4c0c5e17b0bb8fa
hot666666 <hotcs6@naver.com>
2024-06-04 17:52:44 +0900

    feat : create ImageCacheService - delete FileManagerService - create ImageCacheService - create DiskStorage, MemoryStorage which are used in ImageCacheService

--- CocoaTalk/Service/Services.swift
+++ CocoaTalk/Service/Services.swift
@@ -12,6 +12,7 @@ protocol ServiceType {
     var userService: UserServiceType { get set }
     var photoPickerService: PhotoPickerServiceType { get set }
     var uploadService: UploadServiceType { get set }
+    var imageCacheService: ImageCacheServiceType { get set }
 }
 
 class Services: ServiceType {
@@ -19,17 +20,20 @@ class Services: ServiceType {
     var userService: UserServiceType
     var photoPickerService: PhotoPickerServiceType
     var uploadService: UploadServiceType
+    var imageCacheService: ImageCacheServiceType
     
     init(
         authService: AuthenticationServiceType = AuthenticationService(),
         userService: UserServiceType = UserService(dbRepository: UserDBRepository()),
         photoPickerService: PhotoPickerServiceType = PhotoPickerService(),
-        uploadService: UploadServiceType = UploadService(provider: UploadProvider())
+        uploadService: UploadServiceType = UploadService(provider: UploadProvider()),
+        imageCacheService: ImageCacheServiceType = ImageCacheService(memoryStorage: MemoryStorage(), diskStorage: DiskStorage())
     ) {
         self.authService = authService
         self.userService = userService
         self.photoPickerService = photoPickerService
         self.uploadService = uploadService
+        self.imageCacheService = imageCacheService
     }
 }
 
@@ -38,4 +42,5 @@ class StubServices: ServiceType {
     var userService: UserServiceType = StubUserService()
     var photoPickerService: PhotoPickerServiceType = StubPhotoPickerService()
     var uploadService: UploadServiceType = StubUploadService()
+    var imageCacheService: ImageCacheServiceType = StubImageCacheService()
 }
