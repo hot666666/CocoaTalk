0e3b3b2edb1cb786a62af03084bbe4d4f6d38204
hot666666 <hotcs6@naver.com>
2024-06-14 17:12:33 +0900

    fix : update Date extension - AHMDateString

--- CocoaTalk/General/Extension/Date+Extension.swift
+++ CocoaTalk/General/Extension/Date+Extension.swift
@@ -20,12 +20,17 @@ extension Date {
         return dateFormatter.string(from: self)
     }
     
-    var YMDDateString: String {
+    var YMDEString: String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ko_KR")
         dateFormatter.dateFormat = "yyyy년 MM월 dd일 EEEE"
         return dateFormatter.string(from: self)
     }
     
-    
+    var AHMDateString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "a h:mm"
+        return dateFormatter.string(from: self)
+    }
 }
