baf5e74b6c56b89d57b612609a8696423f533bcb
hot666666 <hotcs6@naver.com>
2024-06-16 15:12:57 +0900

    fix : update DIContainer

--- CocoaTalk/Dependency/DIContainer.swift
+++ CocoaTalk/Dependency/DIContainer.swift
@@ -9,11 +9,14 @@ import Foundation
 
 class DIContainer: ObservableObject {
     var services: ServiceType
+    var searchDataController: DataControllable
     var navigationRouter: NavigationRoutable & ObservableObjectSettable
     
     init(services: ServiceType,
+         searchDataController: DataControllable = SearchDataController(),
          navigationRouter: NavigationRoutable & ObservableObjectSettable = NavigationRouter()) {
         self.services = services
+        self.searchDataController = searchDataController
         self.navigationRouter = navigationRouter
         self.navigationRouter.setObjectWillChange(objectWillChange)
     }
